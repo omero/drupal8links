05f63a1d63c83dc07738786d7a1ead4ee8f2c9e3
omers <omersguchigu@gmail.com>
2014-08-29 17:43:06 -0500

    init commit adding angular app with yeoman generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drupal8linksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drupal8linksApp
+ */
+angular.module('drupal8linksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

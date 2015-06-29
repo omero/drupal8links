fa2f155f78b71ccbb3fa8c642e54592033c55846
Omar Aguirre Tenorio <omersguchigu@gmail.com>
2015-06-28 19:48:13 -0500

    update code

--- app/scripts/services/resourcessvc.js
+++ app/scripts/services/resourcessvc.js
@@ -11,12 +11,12 @@ angular.module('drupal8linksApp')
   .factory('resourcesSvc', function () {
     return {
       resources: {
-        connect: 'http://dev-links-drupal-7.pantheon.io/api/system/connect',
+        /*connect: 'http://dev-links-drupal-7.pantheon.io/api/system/connect',
         login: 'http://dev-links-drupal-7.pantheon.io/api/user/login',
-        logout: 'http://dev-links-drupal-7.pantheon.io/api/user/logout',
-        categories: 'http://dev-links-drupal-7.pantheon.io/api/views/categories_links',
-        links:'http://dev-links-drupal-7.pantheon.io/api/views/links',
-        node:'http://dev-links-drupal-7.pantheon.io/api/node'
+        logout: 'http://dev-links-drupal-7.pantheon.io/api/user/logout',*/
+        categories: 'http://links-drupal-8.local/api/catgories'
+        /*links:'http://dev-links-drupal-7.pantheon.io/api/views/links',
+        node:'http://dev-links-drupal-7.pantheon.io/api/node'*/
       }
     };
   });
